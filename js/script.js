ab46d46afde860359605f9f26a43f25a6d88d47c
louisedly <krizia.deloyola1@gmail.com>
2020-07-29 18:26:36 -0400

    Initial commit  :fire:

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Create app namespace to hold all methods
+const movieSearchApp = {};
+// Collect user input using a form to fill in search text 
+movieSearchApp.collectInfo = function () { };
+// Make AJAX request using by using user input + search parameters
+movieSearchApp.getInfo = function () { };
+// Make second AJAX request when clicking "more info" by ID or Title parameters
+movieSearchApp.moreInfo = function () { };
+// Display data on the page
+movieSearchApp.displayInfo = function () { };
+// Start app
+movieSearchApp.init = function () { };
+$(function () {
+    movieSearchApp.init();
+});
