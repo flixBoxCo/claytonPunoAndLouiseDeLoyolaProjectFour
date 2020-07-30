687296ee35b4b07e13756531c65eedc7b79d77a5
Clayton Puno <claytonpuno@Claytons-MacBook-Pro-2.local>
2020-07-30 16:03:21 -0400

    added function for form input

--- js/script.js
+++ js/script.js
@@ -2,32 +2,34 @@ $(document).ready(() => {
     //Doc Ready
     $(function () {
         console.log('doc is ready');
+        movieSearchApp.init();
     });
     // Create app namespace to hold all methods
     const movieSearchApp = {};
+
+    // Start app
+    movieSearchApp.init = function () {
+        movieSearchApp.collectInfo();
+     };
+
     // Collect user input using a form to fill in search text 
-    // $("form").on('submit', (e) => {
-    //     console.log($('#search').val());
-    //     e.preventDefault();
-    // });
     
     movieSearchApp.collectInfo = function () {
         $("form").on('submit', (e) => {
             e.preventDefault();
-            console.log($('#search').val());
+            let searchText = $('#search').val();
+            movieSearchApp.getInfo(searchText);
         });
     };
-
-    movieSearchApp.collectInfo();
-
+    
     // Make AJAX request using by using user input + search parameters
-    movieSearchApp.getInfo = function () { };
+    movieSearchApp.getInfo = function (searchText) {
+        console.log(searchText);
+     };
     // Make second AJAX request when clicking "more info" by ID or Title parameters
     movieSearchApp.moreInfo = function () { };
     // Display data on the page
     movieSearchApp.displayInfo = function () { };
-    // Start app
-    movieSearchApp.init = function () { };
 
 
 
