457d9e23387cf560d2ee71198b09554d5f0f9e66
louisedly <krizia.deloyola1@gmail.com>
2020-08-04 17:18:22 -0400

    Updated js

--- js/script.js
+++ js/script.js
@@ -29,7 +29,7 @@ flixBoxApp.eventListener = function () {
     // Clears search results when user hits backspace
     let input = $('input');
     input.on('keydown', function () {
-        var key = event.keyCode || event.charCode;
+        let key = event.keyCode || event.charCode;
 
         if (key == 8 || key == 46) {
             location.reload();
@@ -58,13 +58,13 @@ flixBoxApp.getMovieName = function (search) {
             apikey: flixBoxApp.apikey,
             s: `${search}`,
         }
-    }).then((result) => {
-        console.log(result);
-        const Search = result.Search;
+    }).then((response) => {
+        // console.log(result);
+        const Search = response.Search;
         flixBoxApp.displayMovie(Search);
     }).catch(error => {
-        alert("Could not find movie. Please enter a valid movie name.")
-        console.log(error);
+        alert("Could not find movie. Please try again")
+        // console.log(error);
     })
 }
 
@@ -73,32 +73,84 @@ flixBoxApp.displayMovie = function(data){
     
     // look through each object in the array
     // get the title, poster and imdbID
-    // display these on the page in html elements
-    // forEach 
+    // display these on the page in html elements using forEach
     data.forEach(function(movies) {
         const movieAll = 
     `
     <div class="movieAll">
-    <a onclick="selectedMovie('${movies.imdbID}')" href= "#">
+    <a onclick="flixBoxApp.selectedMovie('${movies.imdbID}')" href= "#">
         <img src="${movies.Poster}">
         <h3>${movies.Title}</h3>
         <h3>${movies.Year}</h3>
-        <button class="selectedMovie('${movies.imdbID}')">Movie Details</button>
+        <a onclick="flixBoxApp.selectedMovie('${movies.imdbID}')" href="#">Movie Details</a>
     </a>
     </div>
     `
 
     $('.section').append(movieAll);
     $(".resetButton").show();
-    console.log(movies);
+    // console.log(movies);
     })
 }
 
-function selectedMovie (id) {
-    sessionStorage.setItem('movieID', id);
+// function for <a onclick> to save session storage to movie.html
+flixBoxApp.selectedMovie = function (id) {
+    sessionStorage.setItem('movieId', id);
     window.location = 'movie.html';
     return false;
 }
+
+// function for movie.html
+// get imdbID for movie and display on DOM
+flixBoxApp.getMovie = function () {
+    //use session storage to get movie id
+    let movieId = sessionStorage.getItem('movieId');
+    $.ajax({
+        url: `https://www.omdbapi.com/?`,
+        method: 'GET',
+        dataType: 'json',
+        data: {
+            apikey: flixBoxApp.apikey,
+            i: `${movieId}`
+        }
+    }).then((response) => {
+        // console.log(response);
+        let movie = response;
+        // display movie on DOM
+        let output = `
+        <div class="movie">
+            <div class="movieImg">
+                <img src="${movie.Poster}">
+            </div>
+            <div class="movieDetails">
+                <h3>${movie.Title}</h3>
+                <ul>
+                    <li>Genre: ${movie.Genre}</li>
+                    <li>Released: ${movie.Released}</li>
+                    <li>Rated: ${movie.Rated}</li>
+                    <li>IMDB Rating: ${movie.imdbRating}</li>
+                    <li>Director: ${movie.Director}</li>
+                    <li>Writer: ${movie.Writer}</li>
+                    <li>Actors: ${movie.Actors}</li>
+                </ul>
+            </div>
+            <div class="moviePlot">
+                <div class="plotContainer">
+                    <h3>Plot</h3>
+                    ${movie.Plot}
+                    <a href="http://imdb.com/title/${movie.imdbID}" target="_blank">IMDB</a>
+                    <a href="index.html">Go Back to Search</a>
+                </div>
+            </div>
+        </div>
+        `
+        $('section').append(output);
+    }).catch(error => {
+        console.log("There is an error for imdbID");
+    })
+}
+
+
 
 //slow scroll 
 flixBoxApp.scroll = function (element) {
